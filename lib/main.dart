db0128810aad6ea90d189cf2762b2937bfc39848
Earlyalfa <ealfasheilawati@gmail.com>
2024-11-05 11:45:33 +0700

    menyelesaikan tugas

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:plugin_2d/widget/takepicture_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+
+  // Jalankan aplikasi setelah kamera pertama berhasil diinisialisasi.
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakepictureScreen(camera: firstCamera),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
